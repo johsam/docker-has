436ef5a949c0c4f9d147fc1402bba7cbcc4a600d
johsam <j.samuelson64@gmail.com>
2019-02-03 20:44:58 +0100

    First working release

---
+++ hass-config/scripts/zmagnet.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+######################################################################
+#
+#	Catch signals...
+#
+######################################################################
+
+trap 'rm -f "${tmpfile}" > /dev/null 2>&1' 0
+trap "exit 2" 1 2 3 15
+
+tmpfile="/tmp/`basename $0`-$$.tmp"
+
+[ -h "$0" ] && scriptDir=$(dirname `readlink $0`) || scriptDir=$( cd `dirname $0` && pwd)
+
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+
+state=${1}
+now=$(date +%s)
+
+eval $(parse_yaml ${scriptDir}/../secrets.yaml secrets_)
+
+
+curl "http://${secrets_hallonet_ip}:80/hallonet/cgi-bin/zmagnet.cgi?id=7&state=${state}&now=${now}"
